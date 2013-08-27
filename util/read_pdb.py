4cb15cab0d869bfbc17d6ff1882d2b361ef99228
Lenna Peterson <arklenna@gmail.com>
2013-08-27 16:34:40 -0400

    Implement read_pdb

--- util/read_pdb.py
+++ util/read_pdb.py
@@ -1,25 +1,55 @@
 #!/usr/bin/env python
 
-import argparse
+import collections
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class ReadPdb(object):
+class ReadPDB(object):
 
-    def __init__(self):
-        """CONSTRUCTOR"""
+    def __init__(self, pdb_list):
+        """Load list of PDBs for iteration"""
+        self.load_codes(pdb_list)
 
-    @classmethod
-    def commandline(cls, module_args=None):
-        desc = """HELPDESCRIPTION"""
-        a = argparse.ArgumentParser(description=desc)
+    def __iter__(self):
+        """Yield pdbcodes and associated chain list"""
+        for k, v in self.pdb_dict.iteritems():
+            # Replace a list of empty strings with empty list
+            if not any(v):
+                v = list()
+            yield dict(pdbcode=k, chainlist=v)
 
-        args = a.parse_args(module_args)
-        c = cls(**vars(args))
-        return c
+    def load_codes(self, pdb_list):
+        """Load PDB code and chain(s) from space-delimited list"""
+        with open(pdb_list, "rb") as fh:
+            pisces_list = list(fh)
+        pdb_dict = collections.defaultdict(list)
+        for i, line in enumerate(pisces_list):
+            if i == 0:
+                line = self.guess_header(line)
+                if line is None:
+                    continue
+            full_code = line.split()[0]
+            if len(full_code) < 4:
+                raise ValueError("Invalid PDB code %s", full_code)
+            code, chain = self.split_index(full_code, 4)
+            pdb_dict[code].append(chain)
+        # Convert defaultdict to normal dict
+        self.pdb_dict = dict(pdb_dict)
 
+    @staticmethod
+    def split_index(s, i):
+        """Split a string `s` at the specified index `i`"""
+        return s[:i], s[i:]
 
-if __name__ == "__main__":
-    ReadPdb.commandline()
+    @staticmethod
+    def guess_header(line, first_col="IDs", val_len="5"):
+        """
+        Guess whether a `line` is a header based on:
+            first_col: expected first column header
+            val_len:   expected first value length
+        """
+        col = line.split()[0]
+        if col != first_col or len(col) == val_len:
+            return line
