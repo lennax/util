b1d70fb87ee1c8baf1c16e360e56f92a90b4a161
Lenna Peterson <arklenna@gmail.com>
2013-08-28 00:12:38 -0400

    Add range_overlap to util_methods

--- util/util_methods.py
+++ util/util_methods.py
@@ -18,3 +18,13 @@ def head(iterable, n=10):
         print v
         if i > n:
             break
+
+def range_overlap(range1, range2):
+    """
+    Return True if ranges have any overlap, else False
+    """
+    x1, x2 = range1
+    y1, y2 = range2
+    if x2 < x1 or y2 < y1:
+        return None
+    return x1 < y2 and y1 < x2
