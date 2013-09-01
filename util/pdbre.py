a45c5cf7c7a18c946c1a7ebe0887308ce3419dc2
Lenna Peterson <arklenna@gmail.com>
2013-09-01 16:15:09 -0400

    Tweak PDB re and add tests.

--- util/pdbre.py
+++ util/pdbre.py
@@ -4,19 +4,41 @@ import re
 
 
 class pdbre(object):
+
+    # Raw PDB file, PDB code in group 1
+    raw = re.compile(r"""(\d[a-z\d]{3})\.pdb$""", re.IGNORECASE)
+
     # PDB file with or without modifications, PDB code in group 1
     mod = re.compile(r"""
-                         .*?                 # prefix (lazy)
-                         ([0-9][a-z0-9]{3})  # 1 digit, 3 alphanum (group 1)
-                         .*                  # suffix (greedy)
-                         \.pdb$""", re.IGNORECASE | re.VERBOSE)
+                         . *?            # prefix (lazy)
+                         (\d[a-z\d]{3})  # 1 digit, 3 alphanum (group 1)
+                         . *             # suffix (greedy)
+                         \.pdb$          # file extension, EOL
+                     """, re.IGNORECASE | re.VERBOSE)
 
     # PDB file w/ or w/o modifications or biounit suffix, PDB code in group 1
     unit = re.compile(r"""
-                         .*?                 # prefix (lazy)
-                         ([0-9][a-z0-9]{3})  # 1 digit, 3 alphanum (group 1)
-                         .*                  # suffix (greedy)
-                         \.pdb[1-9]?$""", re.IGNORECASE | re.VERBOSE)
+                         . *?            # prefix (lazy)
+                         (\d[a-z\d]{3})  # 1 digit, 3 alphanum (group 1)
+                         . *             # suffix (greedy)
+                         \.pdb           # file extension
+                         [1-9] ? $       # optional digit, EOL
+                      """, re.IGNORECASE | re.VERBOSE)
 
-    # Raw PDB file, PDB code in group 1
-    raw = re.compile(r"""([0-9][a-z0-9]{3})\.pdb""", re.IGNORECASE)
+if __name__ == "__main__":
+
+    test_data = {
+        '1mbn.pdb':       {'raw': 1, 'mod': 1, 'unit': 1},
+        'prefix1mbn.pdb': {'raw': 0, 'mod': 1, 'unit': 1},
+        '1mbnsuffix.pdb': {'raw': 0, 'mod': 1, 'unit': 1},
+        '1mbn_0001.pdb':  {'raw': 0, 'mod': 1, 'unit': 1},
+        '1mbn.pdb1':      {'raw': 0, 'mod': 0, 'unit': 1},
+    }
+
+    for pdb_string, result_dict in test_data.iteritems():
+        for testname, result in result_dict.iteritems():
+            pattern = getattr(pdbre, testname)
+            match = pattern.match(pdb_string)
+            assert bool(match) == bool(result)
+            if match:
+                assert match.group(1) == "1mbn"
