b492fbebcf5bbadbfb4d3dbff4e747bfabc16d63
Lenna Peterson <arklenna@gmail.com>
2014-01-13 13:59:58 -0500

    Convert one_to_full into a class property

--- util/atom_data.py
+++ util/atom_data.py
@@ -1,6 +1,15 @@
 # Copyright 2013 Lenna X. Peterson. All rights reserved.
 
 
+class classprop(object):
+
+    def __init__(self, f):
+        self.f = classmethod(f)
+
+    def __get__(self, *a):
+        return self.f.__get__(*a)()
+
+
 class AtomData(object):
 
     # Maximum ASA for each residue
@@ -55,7 +64,14 @@ class AtomData(object):
         'GLY': 'G', 'PRO': 'P', 'CYS': 'C'}
 
     one_to_three = {o: t for t, o in three_to_one.iteritems()}
-    one_to_full = {o: three_to_full[t] for t, o in three_to_one.iteritems()}
+
+    @classprop
+    def one_to_full(cls):
+        """
+        This can't see three_to_full unless explicitly passed because
+        dict comprehensions create their own local scope
+        """
+        return {o: cls.three_to_full[t] for t, o in cls.three_to_one.iteritems()}
 
     res_atom_list = dict(
         ALA=['C', 'CA', 'CB', 'N', 'O'],
