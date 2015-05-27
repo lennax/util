1a330ec723df327be85cfadc9e14a02f7f683bfd
Lenna Peterson <arklenna@gmail.com>
2015-05-27 14:52:55 -0400

    Count cpus

--- util/util_methods.py
+++ util/util_methods.py
@@ -85,6 +85,40 @@ def bash_wrap(cmd, module=None):
     full_cmd = '/bin/bash -c "{module}{cmd}"'.format(**kwargs)
     return full_cmd
 
+def available_cpu_count():
+    """
+    Number of available virtual or physical CPUs on this system, i.e.
+    user/real as output by time(1) when called with an optimally scaling
+    userspace-only program
+    credit to http://stackoverflow.com/a/1006301
+    """
+
+    # cpuset
+    # cpuset may restrict the number of *available* processors
+    try:
+        m = re.search(r'(?m)^Cpus_allowed:\s*(.*)$',
+                      open('/proc/self/status').read())
+        if m:
+            res = bin(int(m.group(1).replace(',', ''), 16)).count('1')
+            if res > 0:
+                return res
+    except IOError:
+        pass
+
+    # Python 2.6+
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+
+    # http://code.google.com/p/psutil/
+    try:
+        import psutil
+        return psutil.cpu_count()   # psutil.NUM_CPUS on old versions
+    except (ImportError, AttributeError):
+        pass
+
 def venn(left, right):
     """
     Return a 3-tuple of:
